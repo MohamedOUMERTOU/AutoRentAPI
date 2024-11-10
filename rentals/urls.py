ada90a034387279fb1d269e91f00f77177c9de50
MOHAMED <100794138+MohamedOUMERTOU@users.noreply.github.com>
2024-11-10 13:48:42 +0100

    add token generate

--- rentals/urls.py
+++ rentals/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import CarListView,CarCreateView, CarDetailView,CarDeleteView,CarUpdateView, ReservationCreateView, UserReservationsView, ReservationUpdateView, ReservationDeleteView
-
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 urlpatterns = [
     path('cars', CarListView.as_view(), name='car-list'),
     path('cars/<int:id>', CarDetailView.as_view(), name='car-detail'),
@@ -11,4 +11,6 @@ urlpatterns = [
     path('users/<int:user_id>/reservations', UserReservationsView.as_view(), name='user-reservations'),
     path('reservations/<int:id>', ReservationUpdateView.as_view(), name='reservation-update'),
     path('reservations/<int:id>', ReservationDeleteView.as_view(), name='reservation-delete'),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
